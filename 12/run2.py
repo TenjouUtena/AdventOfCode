c4393398478a320896ef282482fffe2b9a845aab
BDS <bds@bds.com>
2016-12-12 14:54:06 -0600

    * Adding addition optimization * Clean up some bad code * divide main loop into functions

--- 12/run2.py
+++ 12/run2.py
@@ -1,12 +1,11 @@
 
 import re, time
 
-start = time.time()
 
-registers = {'a':0, 'b':0, 'c':0, 'd':0}
+registers = {'a':0, 'b':0, 'c':1, 'd':0}
 halt = False
 pointer = 0
-pas = 0
+instructions = []
 
 def value(x):
     if x in registers:
@@ -16,36 +15,60 @@ def value(x):
 
 def cpy(x,y):
     registers[y] = value(x)
-def inc(x,y):
+def inc(x,_):
     registers[x] += 1
-def dec(x,y):
+def dec(x,_):
     registers[x] -= 1
 def jnz(x,y):
     global pointer
     if value(x) != 0:
         pointer += y -1
+def adt(x,y):
+    registers[x] += registers[y]
+    registers[y] = 0
+def nop(*_):
+    pass
+
+def comp():
+    inst = {'cpy':cpy, 'inc':inc, 'dec':dec, 'jnz':jnz, 'adt':adt, 'nop':nop}
+
+    f = open('codes','rb')
+    #test = re.compile(r"([a-z]{3}) ([a-z0-9]+) ?([\-a-z0-9]+)?")
+    for line in f.readlines():
+        mm = re.match(r"([a-z]{3}) ([a-z0-9]+) ?([\-a-z0-9]+)?",line)
+        if mm.lastindex == 2:
+            instructions.append((inst[mm.group(1)],mm.group(2),None))
+        else:
+            opr1 = mm.group(2) if mm.group(2) in registers else int(mm.group(2))
+            opr2 = mm.group(3) if mm.group(3) in registers else int(mm.group(3))
+            instructions.append((inst[mm.group(1)],opr1, opr2))
+    f.close()
+
+def optimize1():
+    ## Optimization Pass 1
+    ## Add register add instruction
+    for xx in xrange(len(instructions)):
+        i,a,b = instructions[xx]
+        if i == jnz and b == -2 :
+            i1,o2,o22 = instructions[xx-1]
+            i2,o3,o33 = instructions[xx-2]
+            if o2 == a and i1 == dec and i2 == inc:
+                instructions[xx-2] = (adt, o3, a)
+                instructions[xx-1] = (nop, None, None)
+                instructions[xx] = (nop, None, None)
+
+def run():
+    global pointer, instructions
+    while pointer < len(instructions):
+        i, a, b = instructions[pointer]
+        i(a,b)
+        pointer += 1
 
 
-inst = {'cpy':cpy, 'inc':inc, 'dec':dec, 'jnz':jnz}
-instructions = []
-f = open('codes','rb')
-#test = re.compile(r"([a-z]{3}) ([a-z0-9]+) ?([\-a-z0-9]+)?")
-for line in f.readlines():
-    mm = re.match(r"([a-z]{3}) ([a-z0-9]+) ?([\-a-z0-9]+)?",line)
-    if mm.lastindex == 2:
-        instructions.append((inst[mm.group(1)],mm.group(2),None))
-    else:
-        opr1 = mm.group(2) if mm.group(2) in registers else int(mm.group(2))
-        opr2 = mm.group(3) if mm.group(3) in registers else int(mm.group(3))
-        instructions.append((inst[mm.group(1)],opr1, opr2))
-f.close()
-
-while pointer < len(instructions):
-    i, a, b = instructions[pointer]
-    i(a,b)
-    pointer += 1
-
-print registers
-print 'Total Time:', time.time() - start
-
-
+if __name__ == "__main__":
+    start = time.time()
+    comp()
+    optimize1()
+    run()
+    print registers
+    print 'Total Time:', time.time() - start
