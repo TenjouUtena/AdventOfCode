fc639c7ab4bb2c1f166dcf798512ae0dda523bf8
BDS <bds@bds.com>
2016-12-14 22:26:50 -0600

    * golf

--- 14/run.py
+++ 14/run.py
@@ -18,29 +18,19 @@ while len(finals) < 64:
     if index % 1000 == 0:
         print index, len(candidates), len(finals)
 
-
-    newcan = []
-    for tt in candidates:
-        if tt[0]+1000 > index:
-            newcan.append(tt)
-    candidates = newcan
+    candidates = filter(lambda x: x[0]+1000 > index, candidates)
 
     curtest = md5(salt + str(index)).hexdigest()
-    for x in xrange(2016):
+    for _ in xrange(2016):
         curtest = md5(curtest).hexdigest()
 
     chars = re.findall(fivere, curtest)
 
-    newcan = []
     for tt in candidates:
         if tt[1] in chars:
-            r, g, h = tt
-            finals.append((r,g,h,index,curtest))
-            print (r,g,h,index,curtest)
-        else:
-            newcan.append(tt)
+            finals.append(tt)
 
-    candidates = newcan
+    candidates = filter(lambda x: not (x[1] in chars), candidates)
 
     m = re.search(threere, curtest)
     if m:
@@ -52,7 +42,5 @@ gg = []
 
 for x in finals:
     gg.append(x[0])
-
-print sorted(gg)
 
 print sorted(gg)[63]
