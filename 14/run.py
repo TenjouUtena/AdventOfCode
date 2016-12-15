97bcd49a9d1e468937904d8eae573a7907f5aa3d
BDS <bds@bds.com>
2016-12-14 22:39:21 -0600

    *More Golf

--- 14/run.py
+++ 14/run.py
@@ -1,9 +1,7 @@
-
 import re
 from hashlib import md5
 
 candidates = []
-
 finals = []
 
 salt = 'yjdafjpo'
@@ -14,22 +12,16 @@ threere = r"(?P<ch>[0-9a-f]+)(?P=ch)(?P=ch)"
 fivere = r"(?P<ch>[0-9a-f]+)(?P=ch)(?P=ch)(?P=ch)(?P=ch)"
 
 while len(finals) < 64:
-
     if index % 1000 == 0:
         print index, len(candidates), len(finals)
 
     candidates = filter(lambda x: x[0]+1000 > index, candidates)
-
     curtest = md5(salt + str(index)).hexdigest()
     for _ in xrange(2016):
         curtest = md5(curtest).hexdigest()
 
     chars = re.findall(fivere, curtest)
-
-    for tt in candidates:
-        if tt[1] in chars:
-            finals.append(tt)
-
+    map(lambda x: finals.append(x) if x[1] in chars else None, candidates)
     candidates = filter(lambda x: not (x[1] in chars), candidates)
 
     m = re.search(threere, curtest)
@@ -38,9 +30,4 @@ while len(finals) < 64:
 
     index += 1
 
-gg = []
-
-for x in finals:
-    gg.append(x[0])
-
-print sorted(gg)[63]
+print sorted(finals)[63]
