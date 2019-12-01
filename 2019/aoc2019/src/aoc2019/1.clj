57d09698a2d8ff88ced2a84316b1eaee89dc8b68
Brendan Speer <brendan.speer@gmail.com>
2019-12-01 11:58:46 -0600

    * Day 1 Pt. 1 & 2 complete

--- 2019/aoc2019/src/aoc2019/1.clj
+++ 2019/aoc2019/src/aoc2019/1.clj
@@ -1,7 +1,23 @@
-(ns aoc2019.1)
+(ns aoc2019.1
+  (:require [clojure.string :as str]))
 
 
+(defn loadff [] (map #(Integer/parseInt %) (str/split-lines (slurp "resources/1.txt"))))
 
 
 (defn fuelcost [weight]
   (- (int (Math/floor (/ weight 3))) 2))
+
+(defn fullfuelcost [w]
+  (loop [we 0
+         f (fuelcost w)]
+    (if (<= f 0)
+      we
+      (recur (+ we f) (fuelcost f)))))
+
+(defn main1 []
+  (reduce + (map fuelcost (loadff))))
+
+
+(defn main2 []
+  (reduce + (map fullfuelcost (loadff))))
